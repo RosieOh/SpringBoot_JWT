e0458ff26fd4e47171438dfa74227150bb4e3c51
RosieOh <20172207@gm.hannam.ac.kr>
2024-01-09 19:34:32 +0900

    🔐document : 공부하면서 기록한 실습 내용🔐

--- me_3.1.5/src/main/java/me/jwttutorial/domain/User.java
+++ me_3.1.5/src/main/java/me/jwttutorial/domain/User.java
@@ -1,23 +1,20 @@
 package me.jwttutorial.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.Set;
 
 @Entity
 @Table(name = "`user`")
-@AllArgsConstructor
 @Getter
-@NoArgsConstructor
+@Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class User {
 
-    @JsonIgnore
     @Id
     @Column(name = "user_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,14 +23,12 @@ public class User {
     @Column(name = "username", length = 50, unique = true)
     private String username;
 
-    @JsonIgnore
     @Column(name = "password", length = 100)
     private String password;
 
     @Column(name = "nickname", length = 50)
     private String nickname;
 
-    @JsonIgnore
     @Column(name = "activated")
     private boolean activated;
 
@@ -41,7 +36,7 @@ public class User {
     @JoinTable(
             name = "user_authority",
             joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
-            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")}
-    )
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
+
     private Set<Authority> authorities;
 }

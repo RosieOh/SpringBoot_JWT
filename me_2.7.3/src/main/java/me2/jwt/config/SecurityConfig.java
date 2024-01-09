408f621349595edf275750709b29c7a7e5f2f7d9
RosieOh <20172207@gm.hannam.ac.kr>
2024-01-09 15:32:04 +0900

    🔐feat : SpringBoot 2.7.3 대응 SpringSecurity+JWT  토큰 생성 및 회원 인가처리🔐

---
+++ me_2.7.3/src/main/java/me2/jwt/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package me2.jwt.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers(
+                        "/h2-console/**",
+                        "/favicon.ico"
+                );
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()        // authorizeRequest : HttpServletRequest를 사용하는 요청들에 대한 접근제한을 설정하겠다는 의미
+                .antMatchers("/api/hello").permitAll()
+                .anyRequest().authenticated();
+    }
+}
---
+++ me_2.7.3/src/test/java/me2/jwt/Me2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me2.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Me2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
